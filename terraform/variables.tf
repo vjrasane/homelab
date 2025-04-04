c70cf19bbe31491eb5f986f35a9abaa48aac193c
Ville Räsänen <git@vjm.anonaddy.me>
2025-04-04 16:59:28 +0300

    fix ansible

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,37 +14,6 @@ variable "pm_api_token_secret" {
 variable "pm_node_name" {
   type = string
 }
-
-variable "lxc_control_node_memory" {
-  type    = number
-  default = 256
-}
-
-variable "lxc_control_node_cores" {
-  type    = number
-  default = 1
-}
-
-variable "lxc_control_node_storage" {
-  type    = number
-  default = 4
-}
-
-variable "lxc_work_node_memory" {
-  type    = number
-  default = 2048
-}
-
-variable "lxc_work_node_cores" {
-  type    = number
-  default = 4
-}
-
-variable "lxc_work_node_storage" {
-  type    = number
-  default = 32
-}
-
 variable "lxc_password" {
   type      = string
   sensitive = true
@@ -57,8 +26,8 @@ variable "lxc_storage" {
 
 variable "lxc_ostemplate" {
   type    = string
-  # default = "local:vztmpl/ubuntu-24.10-standard_24.10-1_amd64.tar.zst"
-  default = "local:vztmpl/debian-12-standard_12.7-1_amd64.tar.zst"
+  default = "local:vztmpl/ubuntu-24.10-standard_24.10-1_amd64.tar.zst"
+  # default = "local:vztmpl/debian-12-standard_12.7-1_amd64.tar.zst"
 }
 
 variable "lxc_default_gateway" {
