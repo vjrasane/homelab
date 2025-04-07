05b2c7bdbb8f39957651e79eb5ac805f554b2d15
Ville Räsänen <git@vjm.anonaddy.me>
2025-04-07 18:57:01 +0300

    add bootstrap script

---
+++ terraform/bootstrap.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -e
+
+# Terraform
+apt update && apt install -y gnupg software-properties-common
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+tee /etc/apt/sources.list.d/hashicorp.list
+
+apt update && apt install -y terraform
+
+terraform -help
+
+# Terraform CDKTF
+npm install --global cdktf-cli@latest
+
+cdktf help
+
+# Kubectl
+apt update && apt-get install -y apt-transport-https ca-certificates curl gnupg 
+mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | \
+gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
+
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | \
+tee /etc/apt/sources.list.d/kubernetes.list
+chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
+
+apt update && apt install -y kubectl
